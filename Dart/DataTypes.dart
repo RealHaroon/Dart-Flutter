6addb5ff939c9ba1cf37264da814fe73edb2a9b8
M Haroon Abbas <haroonreyz@gmail.com>
2026-08-18 13:09:16 +0500

    DataTypes and Opeartors

---
+++ Dart/DataTypes.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+void main() {
+  String greeting = "Hello from Dart!";
+  int year = 2026;
+  double pi=3.14;
+  bool b=true;
+  num n=3.1;
+
+  print('$greeting We are in the year $year.');
+  print("PI Value $pi");
+  print(b);
+  print(n);
+
+  //* Strint Methods
+  String s="haroon abbas khan";
+  print(s.toUpperCase());
+  print(s.toLowerCase());
+  print(s.substring(3,6));
+  print(s.split(""));
+
+}
---
+++ Dart/Operators.dart
@@ -0,0 +1,27 @@
+void main(){
+
+  int a=10;
+  int b=4;
+
+  int sum=a+b;
+  int diff=a-b;
+  int prod=a*b;
+  double div=a/b;
+  int floordiv=a~/b;
+
+  print("Sum of $a and $b is : $sum");
+  print("Differnece of $a and $b is : $diff");
+  print("Product of $a and $b is : $prod");
+  print("Division of $a and $b is : $div");
+  print("Floor Division of $a and $b is : $floordiv");
+
+
+  int num1=50;
+  int num2=40;
+  num1=num1+num2;
+  num2=num1-num2;
+  num1=num1-num2;
+  print(num1);
+  print(num2);
+
+}
