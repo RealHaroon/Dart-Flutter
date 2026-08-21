b48e89e27bbd795e76d2df85c34645090248b777
M Haroon Abbas <haroonreyz@gmail.com>
2026-08-21 22:45:06 +0500

    Variables

--- Dart/Operators.dart
+++ Dart/Operators.dart
@@ -1,6 +1,6 @@
 
 
-import 'dart:io';
+// import 'dart:io';
 
 void main(){
 
--- Dart/UserInput.dart
+++ Dart/UserInput.dart
@@ -5,7 +5,7 @@ void main(){
   int n1=int.parse(stdin.readLineSync()!);
   stdout.write("Enter Number 2 :");
   int n2=int.parse(stdin.readLineSync()!);
-
+  // parse() can be used with int and double too
   // ! null check means null can in taken 
   // ? null-allow means null can be taken
 
